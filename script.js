34145b1581981e75579e65af10926b2495ad094b
Anushree Nema <anushreenema624@gmail.com>
2025-07-02 21:58:23 +0530

    configuration setup

---
+++ script.js
@@ -0,0 +1,5 @@
+// setting up configurations
+var studentToken = "90935021|-31949211575688821|90959345";
+var studentDB = "SCHOOL-DB";
+var studentRel = "STUDENT-TABLE";
+var studentBaseUrl = "http://api.login2explore.com:5577";
